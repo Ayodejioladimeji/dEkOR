1b543e86df4fcb34d6738832bbf0df6aa22c5680
Ayodejioladimeji <layobright11@gmail.com>
2024-07-07 12:40:42 +0100

    implemented the product details image and content

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -2,29 +2,75 @@ import Breadcumb from "@/common/breadcumb";
 import Loading from "@/common/loading";
 import Layout from "@/components/Layout";
 import { data } from "@/constants/data";
+import { ACTIONS } from "@/store/Actions";
+import { DataContext } from "@/store/GlobalState";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Ratings } from "../../../public/assets";
 
 interface Props {}
 
 const Product = (props: Props) => {
-  const [product, setProduct] = useState(null);
   const router = useRouter();
   const { slug } = router.query;
+  const [product, setProduct] = useState(null);
   const [loading, setLoading] = useState(true);
+  const { state, dispatch } = useContext(DataContext);
+  const [productColor, setProductColor] = useState("");
+  const [imageIndex, setImageIndex] = useState(0);
 
   //  get detail product
   useEffect(() => {
     if (slug) {
-      data?.forEach((item) => {
+      data?.find((item) => {
+        console.log(item.id, slug);
         if (item.id === slug) {
           setProduct(item);
+          setLoading(false);
         }
       });
-      setLoading(false);
     }
   }, [slug]);
+
+  // increase item
+  const increment = () => {
+    state?.cart.forEach((item) => {
+      if (item.id === product?.id) {
+        item.quantity += 1;
+      }
+    });
+
+    const carting = state?.cart.find((item) => item.id === product?.id);
+
+    const cartData = {
+      ...product,
+      quantity: carting?.quantity,
+    };
+
+    //  dispatch({ type: ACTIONS.CART, payload: cartData });
+  };
+
+  // // decrease cart items
+  const decrement = () => {
+    state?.cart.forEach((item) => {
+      if (item.id === product.id) {
+        if (item.quantity === 1) return;
+        item.quantity -= 1;
+      }
+    });
+
+    const carting = state?.cart.find((item) => item.id === product.id);
+
+    const cartData = {
+      ...product,
+      quantity: carting?.quantity,
+    };
+
+    //  dispatch({ type: ACTIONS.CART, payload: cartData });
+  };
+
+  console.log(product?.images);
 
   //
 
@@ -49,14 +95,85 @@ const Product = (props: Props) => {
             <div className="content">
               <div className="row">
                 <div className="col">
-                  <Image
-                    src={product?.images[0]}
-                    alt="product-image"
-                    width={100}
-                    height={100}
-                  />
+                  <div className="row">
+                    <div className="col-9">
+                      <div className="detail-image">
+                        <Image
+                          src={product?.images && product?.images[imageIndex]}
+                          alt="product-image"
+                          width={100}
+                          height={100}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-3">
+                      <div className="thumb">
+                        {product?.images?.map((img, index) => (
+                          <div
+                            className={`image-box ${
+                              imageIndex === index ? "image-active" : ""
+                            }`}
+                            key={index}
+                          >
+                            <Image
+                              src={img}
+                              alt=""
+                              onClick={() => setImageIndex(index)}
+                              width={100}
+                              height={100}
+                            />
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="col">right</div>
+
+                <div className="col">
+                  <div className="title">
+                    <h1>{product?.title}</h1>
+                    <Ratings />
+                  </div>
+                  <p>{product?.description}</p>
+
+                  <h3>${product?.price}</h3>
+
+                  <div className="color-div">
+                    {product?.colors?.map((color, index) => {
+                      return (
+                        <div
+                          key={index}
+                          onClick={() => setProductColor(color)}
+                          className={
+                            productColor === color
+                              ? "active-color"
+                              : "main-color"
+                          }
+                        >
+                          <div
+                            className="actual-color"
+                            style={{ background: color }}
+                          ></div>
+                        </div>
+                      );
+                    })}
+                  </div>
+
+                  <h4>Quantity</h4>
+                  <div className="quantities">
+                    <i className="bi bi-dash" onClick={decrement}></i>
+
+                    <p>{product?.quantity}</p>
+
+                    <i className="bi bi-plus" onClick={increment}></i>
+                  </div>
+
+                  <div className="button-container">
+                    <button>Add To Cart</button>
+                    <button>Checkout</button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
