a591888cbed6f8d918e0624be1376a2ff62b2136
Ayodejioladimeji <layobright11@gmail.com>
2024-07-07 13:07:37 +0100

    completed information section

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -19,6 +19,7 @@ const Product = (props: Props) => {
   const { state, dispatch } = useContext(DataContext);
   const [productColor, setProductColor] = useState("");
   const [imageIndex, setImageIndex] = useState(0);
+  const [count, setCount] = useState(4);
 
   //  get detail product
   useEffect(() => {
@@ -174,6 +175,81 @@ const Product = (props: Props) => {
                     <button>Checkout</button>
                   </div>
                 </div>
+              </div>
+            </div>
+
+            <hr />
+
+            {/* more information section */}
+            <div className="information">
+              <div className="top">
+                <button
+                  onClick={() => setCount(1)}
+                  className={count === 1 ? "active" : ""}
+                >
+                  Description
+                </button>
+                <button
+                  onClick={() => setCount(2)}
+                  className={count === 2 ? "active" : ""}
+                >
+                  Shipping
+                </button>
+                <button
+                  onClick={() => setCount(3)}
+                  className={count === 3 ? "active" : ""}
+                >
+                  Reviews
+                </button>
+                <button
+                  onClick={() => setCount(4)}
+                  className={count === 4 ? "active" : ""}
+                >
+                  Return Policy
+                </button>
+              </div>
+
+              <div className="bottom">
+                {count === 1 && (
+                  <div className="description">
+                    <p> Item Description</p>
+                  </div>
+                )}
+
+                {count === 2 && (
+                  <div className="shipping">
+                    <p>Shipping Information</p>
+                  </div>
+                )}
+
+                {count === 3 && (
+                  <div className="reviews">
+                    <p> Item Review</p>
+                  </div>
+                )}
+
+                {count === 4 && (
+                  <div className="policy">
+                    <h4>Our Return Policy and Guarantee</h4>
+                    <p>
+                      Exchange within 30 days from the date product was
+                      delivered
+                    </p>
+
+                    <h4>Requirement</h4>
+                    <ol>
+                      <li>
+                        Items shouldn’t stay more than 30 days from delivery
+                        date.
+                      </li>
+                      <li>
+                        Items should remain unused,not damaged and in the
+                        original package.
+                      </li>
+                      <li>Shipping fees won’t be covered by the company.</li>
+                    </ol>
+                  </div>
+                )}
               </div>
             </div>
           </div>
