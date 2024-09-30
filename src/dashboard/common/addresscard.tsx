20f330602bc370aa01afa23dd6c59441115092be
Ayodejioladimeji <layobright11@gmail.com>
2024-09-30 08:14:39 +0100

    Implemented a confirm modal component.

--- src/dashboard/common/addresscard.tsx
+++ src/dashboard/common/addresscard.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import ConfirmModal from "./confirmmodal";
 //
 
 interface Props {
@@ -12,25 +13,49 @@ interface Props {
 }
 
 const AddressCard = (props: Props) => {
+  const [confirm, setConfirm] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+    setLoading(true);
+    setLoading(false);
+  };
+
   //
+
   return (
-    <div className="address-card">
-      <div>
-        <h3>{props?.name}</h3>
-        <p>{props?.address}</p>
-        <p>{props?.region}</p>
-        <p>{props?.city}</p>
-        <p>{props?.phone}</p>
+    <>
+      <div className="address-card">
+        <div onClick={() => setConfirm(true)}>
+          <h3>{props?.name}</h3>
+          <p>{props?.address}</p>
+          <p>{props?.region}</p>
+          <p>{props?.city}</p>
+          <p>{props?.phone}</p>
 
-        {props?.isDefault && <i className="bi bi-check-circle-fill"></i>}
+          {props?.isDefault && <i className="bi bi-check-circle-fill"></i>}
+        </div>
+
+        <hr />
+        <div className="address-below">
+          <i className="bi bi-trash3"></i>
+          <i className="bi bi-pencil-square"></i>
+        </div>
       </div>
 
-      <hr />
-      <div className="address-below">
-        <i className="bi bi-trash3"></i>
-        <i className="bi bi-pencil-square"></i>
-      </div>
-    </div>
+      {confirm && (
+        <ConfirmModal
+          title="Default Address"
+          subtitle="Are you sure you want to set this address to your default address?"
+          buttonTitle="Save"
+          buttonColor="#27493e"
+          onSubmit={handleSubmit}
+          loading={loading}
+          setConfirmModal={setConfirm}
+          confirmModal={confirm}
+        />
+      )}
+    </>
   );
 };
 
