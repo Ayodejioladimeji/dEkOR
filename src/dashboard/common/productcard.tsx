42bc04b03fde46dfa8aff5d42767796d4a2805ff
Ayodejioladimeji <layobright11@gmail.com>
2024-10-02 21:17:05 +0100

    Implemented delete product modal.

--- src/dashboard/common/productcard.tsx
+++ src/dashboard/common/productcard.tsx
@@ -2,15 +2,18 @@ import { ACTIONS } from "@/store/Actions";
 import { DataContext } from "@/store/GlobalState";
 import cogoToast from "cogo-toast";
 // import { useRouter } from "next/router";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Image } from "react-bootstrap";
 import { DeleteFavourite } from "../../../public/assets";
 import { firstTwoWords, formatMoney } from "@/utils/utils";
+import ConfirmModal from "./confirmmodal";
 
 //
 
 const Productcard = (props: any) => {
   const { state, dispatch } = useContext<any>(DataContext);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [deleteloading, setDeleteloading] = useState(false);
 
   // add items to cart
   const addToCart = () => {
@@ -30,34 +33,55 @@ const Productcard = (props: any) => {
       dispatch({ type: ACTIONS.CART, payload: cartData });
     } else {
       cogoToast.error("Item already added to your cart");
+      setDeleteloading(false);
     }
+  };
+
+  // handle delete
+  const handleDelete = () => {
+    console.log("submitted");
   };
 
   //
   return (
-    <div className="order-card">
-      <div className="order-image">
-        <Image
-          src={props?.images[0]}
-          alt="product-image"
-          width={100}
-          height={100}
-        />
+    <>
+      <div className="order-card">
+        <div className="order-image">
+          <Image
+            src={props?.images[0]}
+            alt="product-image"
+            width={100}
+            height={100}
+          />
 
-        <div className="item-cart" onClick={addToCart}>
-          <DeleteFavourite />
+          <div className="item-cart" onClick={() => setDeleteModal(true)}>
+            <DeleteFavourite />
+          </div>
+
+          <button onClick={addToCart} className="add-to-cart">
+            Edit Product
+          </button>
         </div>
 
-        <button onClick={addToCart} className="add-to-cart">
-          Edit Product
-        </button>
+        <div className="order-content">
+          <h3>{firstTwoWords(props?.title)}</h3>
+          <p>${formatMoney(Number(props?.price))}</p>
+        </div>
       </div>
 
-      <div className="order-content">
-        <h3>{firstTwoWords(props?.title)}</h3>
-        <p>${formatMoney(Number(props?.price))}</p>
-      </div>
-    </div>
+      {deleteModal && (
+        <ConfirmModal
+          title="Remove Product"
+          subtitle="Are you sure you want to remove this product?"
+          buttonTitle="Remove"
+          buttonColor="red"
+          onSubmit={handleDelete}
+          loading={deleteloading}
+          setConfirmModal={setDeleteModal}
+          confirmModal={deleteModal}
+        />
+      )}
+    </>
   );
 };
 
