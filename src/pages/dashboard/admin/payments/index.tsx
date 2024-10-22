e8799a746780006c439534b3d55ac1c3fe27d697
Ayodejioladimeji <layobright11@gmail.com>
2024-10-22 22:58:04 +0100

    Implemented transaction pagination on the client side.

--- src/pages/dashboard/admin/payments/index.tsx
+++ src/pages/dashboard/admin/payments/index.tsx
@@ -4,18 +4,36 @@ import Topbar from "@/dashboard/components/topbar";
 import Paymentcard from "@/dashboard/common/paymentcard";
 import PaymentSkeleton from "@/common/paymentskeleton";
 import { GetRequests } from "@/utils/requests";
+import Paginate from "@/components/pagination/Paginate";
+import { useRouter } from "next/router";
+
+/* eslint-disable */
 
 const Payments = () => {
   const [loading, setLoading] = useState(true);
   const [payments, setPayments] = useState<any>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalCount, setTotalCount] = useState(0);
+  const PageSize = 20;
+  const router = useRouter();
+  const { page } = router.query;
 
   useEffect(() => {
     const token = localStorage.getItem("token") || "";
 
     const getPayments = async () => {
-      const res = await GetRequests("/transactions/admin", token);
+      const res = await GetRequests(
+        `/transactions/admin?page=${page === undefined ? currentPage : page}&pageSize=${PageSize}`,
+        token
+      );
       if (res?.status === 200) {
         setPayments(res?.data);
+        setTotalCount(res?.data?.totalCount);
+
+        if (page === undefined) {
+          setCurrentPage(1);
+        }
+
         setLoading(false);
       } else {
         setLoading(false);
@@ -23,7 +41,7 @@ const Payments = () => {
     };
 
     getPayments();
-  }, []);
+  }, [page]);
   //
 
   return (
@@ -59,6 +77,25 @@ const Payments = () => {
                 style={{ fontSize: "45px" }}
               ></i>
               You have no payments available
+            </div>
+          )}
+
+          {/* pagination */}
+          {!loading && payments?.length !== 0 && totalCount > PageSize && (
+            <div className="page-navigation">
+              <div className="mt-3">
+                <Paginate
+                  className="pagination-bar"
+                  currentPage={
+                    !loading && page === undefined ? currentPage : Number(page)
+                  }
+                  totalCount={totalCount}
+                  pageSize={PageSize}
+                  onPageChange={(page) => setCurrentPage(page)}
+                  loading={loading}
+                  setLoading={setLoading}
+                />
+              </div>
             </div>
           )}
         </div>
